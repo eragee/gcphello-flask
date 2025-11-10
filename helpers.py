3260b28bac2de2f2f45eebfd6f3008c7876fe88b
Erik Agee <erik.r.agee@gmail.com>
2025-11-10 15:55:32 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,12 +1,13 @@
 from flask import jsonify
 
-def restresponse(message: str):
-    """
-    Formats a string into a standardized JSON response.
-    Example:
-        {"status": "OK", "result": "Hello, World!"}
-    """
+def rest_response(message: str):
     return jsonify({
         "status": "OK",
         "result": message
     })
+
+def rest_error(message: str):
+    return jsonify({
+        "status": "ERROR",
+        "result": message
+    }), 400
