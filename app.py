e106842ee78bee69bb8a768b43e7c0fc5796272a
Erik Agee <erik.r.agee@gmail.com>
2025-11-10 16:03:57 -0800

    Fix rest_response references

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from helpers import restresponse
+from helpers import rest_response
 
 app = Flask(__name__)
 app.json.sort_keys = False
 
 @app.route("/hello")
 def hello():
-    return restresponse("Hello World")
+    return rest_response("Hello World")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
